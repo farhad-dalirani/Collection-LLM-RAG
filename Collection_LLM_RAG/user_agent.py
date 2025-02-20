bb87ee07372b1efbcb61968f615dae19232ae03b
fd <dalirani.1373@gmail.com>
2025-02-20 12:15:00 -0500

    Add system prompt for RAG usage of llms

--- Collection_LLM_RAG/user_agent.py
+++ Collection_LLM_RAG/user_agent.py
@@ -6,6 +6,7 @@ from llama_index.core.tools import QueryEngineTool
 from llama_index.core.memory import ChatMemoryBuffer
 from llama_index.core.query_engine import RouterQueryEngine
 from llama_index.core.selectors import PydanticMultiSelector
+from llama_index.core.query_engine import SubQuestionQueryEngine
 from openai import AuthenticationError
 
 from knowledgeBase.hybrid_query_engine import load_hybrid_query_engine
@@ -57,6 +58,21 @@ class UserAgent:
             self.set_llm(llm_name)
             self.set_embd(embedding_name)
 
+        self.system_message = """You are an AI teacher, answering questions from students of an applied AI course on Large Language Models (LLMs or llm) and Retrieval Augmented Generation (RAG) for LLMs.
+Topics covered include training models, fine-tuning models, giving memory to LLMs, prompting tips, hallucinations and bias, vector databases, transformer architectures, embeddings, RAG frameworks such as Langchain and LlamaIndex, making LLMs interact with tools, AI agents, reinforcement learning with human feedback (RLHF). Questions should be understood in this context.
+Your answers are aimed to teach students, so they should be complete, clear, and easy to understand.
+Use the available tools to gather insights pertinent to the field of AI.
+To answer student questions, always use the all_sources_info tool.
+Only some information returned by the tools might be relevant to the question, so ignore the irrelevant part and answer the question with what you have.
+Your responses are exclusively based on the output provided by the tools. Refrain from incorporating information not directly obtained from the tool's responses.
+When the conversation deepens or shifts focus within a topic, adapt your input to the tools to reflect these nuances. This means if a user requests further elaboration on a specific aspect of a previously discussed topic, you should reformulate your input to the tool to capture this new angle or more profound layer of inquiry.
+Provide comprehensive answers, ideally structured in multiple paragraphs, drawing from the tool's variety of relevant details. The depth and breadth of your responses should align with the scope and specificity of the information retrieved.
+Should the tools repository lack information on the queried topic, politely inform the user that the question transcends the bounds of your current knowledge base, citing the absence of relevant content in the tool's documentation.
+At the end of your answers, always invite the students to ask deeper questions about the topic if they have any. Make sure reformulate the question to the tool to capture this new angle or more profound layer of inquiry.
+Do not refer to the documentation directly, but use the information provided within it to answer questions.
+If code is provided in the information, share it with the students. It's important to provide complete code blocks so they can execute the code when they copy and paste them.
+Make sure to format your answers in Markdown format, including code blocks and snippets.
+"""
 
     def interact_with_agent(self, message, chat_history):
         """
@@ -116,7 +132,7 @@ class UserAgent:
                     # Handle the case where source_nodes isn't available
                     logging.info("Warning: 'source_nodes' attribute not found in raw_output.")
         
-        elif self.mode == "Router-Based Query Engines":    
+        elif self.mode in ["Router-Based Query Engines", "SubQuestion-Based Query Engines"]:    
             # Send the user's message to the Router Query Engine
             try:
                 response = self.agent.query(message)
@@ -186,11 +202,12 @@ class UserAgent:
         Raises:
         ValueError: If the provided LLM name is not supported.
         """
+
         self.llm_name = llm_name
         if self.llm_name == 'OpenAI GPT-4o mini':
-            self.model_llm = OpenAI(model="gpt-4o-mini", temperature=self.temperature, api_key=self.openAI_api)
+            self.model_llm = OpenAI(model="gpt-4o-mini", temperature=self.temperature, api_key=self.openAI_api, system_prompt=self.system_message)
         elif self.llm_name == 'OpenAI GPT-4o':
-            self.model_llm = OpenAI(model="gpt-4o", temperature=self.temperature, api_key=self.openAI_api)
+            self.model_llm = OpenAI(model="gpt-4o", temperature=self.temperature, api_key=self.openAI_api, system_prompt=self.system_message)
         else:
             raise ValueError('Selected LLM name is not supported.')
 
@@ -269,6 +286,12 @@ class UserAgent:
                             llm=self.model_llm,
                             verbose=True
                         )
+        elif self.mode == "SubQuestion-Based Query Engines":
+            self.agent = SubQuestionQueryEngine.from_defaults(
+                query_engine_tools=qs_list,
+                llm=self.model_llm,
+                verbose=True
+            )
         else:
            raise ValueError('Selected mode is not supported.')
 
