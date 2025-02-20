1889796576fbfdd948e3f4f4b1d4e01ade30dce2
fd <dalirani.1373@gmail.com>
2025-02-20 15:49:20 -0500

    optional system prompt

--- Collection_LLM_RAG/user_agent.py
+++ Collection_LLM_RAG/user_agent.py
@@ -38,7 +38,7 @@ class UserAgent:
         set_api(openAI_api):
             Sets the OpenAI API key and reinitializes the models and agent.
     """
-    def __init__(self, llm_name, embedding_name, openAI_api, mode, query_engines_details=[], temperature=0):
+    def __init__(self, llm_name, embedding_name, openAI_api, mode, query_engines_details=[], temperature=0, system_message=None):
         
         self.llm_name = llm_name
         self.embedding_name = embedding_name
@@ -58,21 +58,24 @@ class UserAgent:
             self.set_llm(llm_name)
             self.set_embd(embedding_name)
 
-        self.system_message = """You are an AI teacher, answering questions from students of an applied AI course on Large Language Models (LLMs or llm) and Retrieval Augmented Generation (RAG) for LLMs.
-Topics covered include training models, fine-tuning models, giving memory to LLMs, prompting tips, hallucinations and bias, vector databases, transformer architectures, embeddings, RAG frameworks such as Langchain and LlamaIndex, making LLMs interact with tools, AI agents, reinforcement learning with human feedback (RLHF). Questions should be understood in this context.
-Your answers are aimed to teach students, so they should be complete, clear, and easy to understand.
-Use the available tools to gather insights pertinent to the field of AI.
-To answer student questions, always use the all_sources_info tool.
-Only some information returned by the tools might be relevant to the question, so ignore the irrelevant part and answer the question with what you have.
-Your responses are exclusively based on the output provided by the tools. Refrain from incorporating information not directly obtained from the tool's responses.
-When the conversation deepens or shifts focus within a topic, adapt your input to the tools to reflect these nuances. This means if a user requests further elaboration on a specific aspect of a previously discussed topic, you should reformulate your input to the tool to capture this new angle or more profound layer of inquiry.
-Provide comprehensive answers, ideally structured in multiple paragraphs, drawing from the tool's variety of relevant details. The depth and breadth of your responses should align with the scope and specificity of the information retrieved.
-Should the tools repository lack information on the queried topic, politely inform the user that the question transcends the bounds of your current knowledge base, citing the absence of relevant content in the tool's documentation.
-At the end of your answers, always invite the students to ask deeper questions about the topic if they have any. Make sure reformulate the question to the tool to capture this new angle or more profound layer of inquiry.
-Do not refer to the documentation directly, but use the information provided within it to answer questions.
-If code is provided in the information, share it with the students. It's important to provide complete code blocks so they can execute the code when they copy and paste them.
-Make sure to format your answers in Markdown format, including code blocks and snippets.
-"""
+        if system_message is None:
+            self.system_message = """You are an AI teacher, answering questions from students of an applied AI course on Large Language Models (LLMs or llm) and Retrieval Augmented Generation (RAG) for LLMs.
+    Topics covered include training models, fine-tuning models, giving memory to LLMs, prompting tips, hallucinations and bias, vector databases, transformer architectures, embeddings, RAG frameworks such as Langchain and LlamaIndex, making LLMs interact with tools, AI agents, reinforcement learning with human feedback (RLHF). Questions should be understood in this context.
+    Your answers are aimed to teach students, so they should be complete, clear, and easy to understand.
+    Use the available tools to gather insights pertinent to the field of AI.
+    To answer student questions, always use the all_sources_info tool.
+    Only some information returned by the tools might be relevant to the question, so ignore the irrelevant part and answer the question with what you have.
+    Your responses are exclusively based on the output provided by the tools. Refrain from incorporating information not directly obtained from the tool's responses.
+    When the conversation deepens or shifts focus within a topic, adapt your input to the tools to reflect these nuances. This means if a user requests further elaboration on a specific aspect of a previously discussed topic, you should reformulate your input to the tool to capture this new angle or more profound layer of inquiry.
+    Provide comprehensive answers, ideally structured in multiple paragraphs, drawing from the tool's variety of relevant details. The depth and breadth of your responses should align with the scope and specificity of the information retrieved.
+    Should the tools repository lack information on the queried topic, politely inform the user that the question transcends the bounds of your current knowledge base, citing the absence of relevant content in the tool's documentation.
+    At the end of your answers, always invite the students to ask deeper questions about the topic if they have any. Make sure reformulate the question to the tool to capture this new angle or more profound layer of inquiry.
+    Do not refer to the documentation directly, but use the information provided within it to answer questions.
+    If code is provided in the information, share it with the students. It's important to provide complete code blocks so they can execute the code when they copy and paste them.
+    Make sure to format your answers in Markdown format, including code blocks and snippets.
+    """
+        else:
+            self.system_message = system_message
 
     def interact_with_agent(self, message, chat_history):
         """
